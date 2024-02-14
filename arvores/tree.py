16a7725723af2ef64306d79b7fe4762bb00a8217
Carlos Sousa <103946120+kcarww@users.noreply.github.com>
2024-02-14 15:50:31 -0300

    Update tree.py

--- arvores/tree.py
+++ arvores/tree.py
@@ -1,1 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
+    def __str__(self):
+        return str(self.data)
+
+
+class BinaryTree:
+    def __init__(self, data=None):
+        if data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
+
+    def simetric_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.simetric_traversal(node.left)
+        print(node)
+        if node.right:
+            self.simetric_traversal(node.right)
+
+if __name__ == "__main__":
+    tree = BinaryTree(7)
+    tree.root.left = Node(3)
+    tree.root.right = Node(15)
+    print(tree.root)
